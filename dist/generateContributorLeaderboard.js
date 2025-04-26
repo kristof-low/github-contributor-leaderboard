f05bac1689ffd17cb1b9cf830750062f40aa6203
christof louw <louw.christof@gmail.com>
2025-04-26 17:43:34 +0200

    initial commit

---
+++ dist/generateContributorLeaderboard.js
@@ -0,0 +1,8 @@
+export function generateContributorLeaderboard(contributors) {
+    let leaderboard = `\n\n| Rank | Contributor | Contributions |\n|------|-------------|----------------|\n`;
+    contributors.forEach((contributor, i) => {
+        const medal = ["🥇", "🥈", "🥉"][i] || `${i + 1}`;
+        leaderboard += `| ${medal} | [${contributor.login}](${contributor.html_url}) | ${contributor.contributions} |\n`;
+    });
+    return leaderboard;
+}
